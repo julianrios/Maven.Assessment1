6f8eefb3224ff874a9950b7cd2796136d1426d71
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-18 09:44:55 -0500

    added basicArrayUtils and basicStringUtils all test completed

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,8 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+        str = str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+        return str;
     }
 
     /**
@@ -17,7 +18,9 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        StringBuilder reverseStr = new StringBuilder(str);
+        reverseStr.reverse();
+        return reverseStr.toString();
     }
 
     /**
@@ -25,7 +28,11 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        str = camelCase(str);
+        StringBuilder reverseStr = new StringBuilder(str);
+        reverseStr.reverse();
+        str = camelCase(reverseStr.toString());
+        return str;
     }
 
 
@@ -34,7 +41,8 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        str = str.substring(1,str.length()-1);
+        return str;
     }
 
     /**
@@ -42,6 +50,18 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char[] charArray = str.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            if(Character.isUpperCase(charArray[i])) {
+                charArray[i] = Character.toLowerCase(charArray[i]);
+            } else if(Character.isLowerCase(charArray[i])) {
+                charArray[i] = Character.toUpperCase(charArray[i]);
+            }
+        }
+        str = new String(charArray);
+        return str;
     }
+
 }
